f697febf7f9b58e5bf7c64cf6cd542857a74c475
Ben Stockil <benstock555@gmail.com>
2023-09-18 10:23:36 +0100

    Create Programming challenge.py

---
+++ Programming challenge.py
@@ -0,0 +1,53 @@
+def Menu():
+    MenuInt = int(input("""Press 1 for Area of triangle,
+Press 2 for Power Calculator,
+Press 3 for Sum of two numbers,
+Press 4 for Convert age to days,
+Press 5 for Encode Morse,
+Press 6 for Calculate profit."""))
+    if MenuInt == 1:
+        TriArea()
+    elif MenuInt == 2:
+        PowerCalc()
+    elif MenuInt == 3:
+        TwoSum()
+
+               
+#Area of a Triangle
+def TriArea():
+    base = int(input("Base of triangle?"))
+    height = int(input("Height of triangle?"))
+    area = int(base * height / 2)
+    print (area)
+    Menu()
+
+def PowerCalc():
+    volt = int(input("Voltage?"))
+    current = int(input("Current?"))
+    power = int(volt * current)
+    print(power)
+    Menu()
+
+def TwoSum():
+    num1 = int(input("Enter first num"))
+    num2 = int(input("Enter second num"))
+    add = int(num1 + num2)
+    if num1 % 7 == 0 or num2 %7 == 0:
+        div = int(num1 / num2)
+        print(div)
+    elif num1 % 11 == 0 or num2 % 11 == 0:
+        mult = int(num1 * num2)
+        print(mult)
+    else:
+        print(add)
+    Menu()
+
+def AgeConvert():
+    age = -1
+    while age < 0:
+        age = int(input("Enter age"))
+    days = age * 365
+    print(days)
+    Menu()
+
+Menu()
